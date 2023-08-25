96593dfc5311efbbf6f3b3c98ecb344cbfee2011
Urvika-gola <urvika.gola@gmail.com>
2023-08-24 21:58:08 -0700

    Q8 Part A
    Signed-off-by: Urvika-gola <urvika.gola@gmail.com>

--- implementation/baselines/language_detector.py
+++ implementation/baselines/language_detector.py
@@ -29,15 +29,24 @@ def detect_language(sentence):
     #   You will get full credit as long as your implementation gets more than 85% accuracy
     #   Your implementation should be simple: a few if statements is all you need
     #   We will test your code with new sentences, so write a generic language detector
+    en_frequent_word_set = {"the", "and", "is", "in", "of", "an"}
+    es_frequent_word_set = {"el", "es", "en", "un", "de", "la", "se"}
 
-    if random.random() < 0.5:
+    en_word_count = sum(1 for word in sentence.split() if word.lower() in en_frequent_word_set)
+    es_word_count = sum(1 for word in sentence.split() if word.lower() in es_frequent_word_set)
+
+    es_letters = any(char in "ñáéíóú¡¿" for char in sentence.lower())
+
+    if en_word_count >= es_word_count:  # if the frequent english words are more than frequent spanish words
         language = "en"
-    else:
+    elif es_letters:  # if it contains spanish letters then surely its spanish
         language = "es"
-
+    elif en_word_count < es_word_count:  # if the frequent spanish words are more than frequent english words
+        language = "es"
+    else:  # if none of the conditions match then default language is english as its more commonly used than spanish
+        language = "en"
     assert language in ["en", "es"]
     return language
-
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Detect the language of document.')
